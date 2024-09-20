ce13bcc048afb8a8554467ead1efc72cf257ab8b
Dustin Dobervich <ddobervich@gmail.com>
2024-09-20 15:35:55 -0500

    initialize tracing

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -13,6 +13,8 @@ pub struct Program {
 
 impl Program {
     pub fn load_from_file(path: impl AsRef<Path>) -> anyhow::Result<Self> {
+        tracing::debug!("loading program from path {:?}", path.as_ref());
+
         let data = std::fs::read(path).context("failed to load CHIP-8 program from disk")?;
 
         Ok(Self { data })
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Context;
 use chipate::{core::Program, Emu};
 use clap::Parser;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::EnvFilter;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -10,7 +12,22 @@ struct Args {
 }
 
 fn main() -> anyhow::Result<()> {
-    let args = Args::try_parse().context("parse command line arguments")?;
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_level(true)
+        .with_target(true)
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+
+    let args = Args::try_parse().context("failed to parse command line arguments")?;
 
     let program = Program::load_from_file(args.file)?;
 
